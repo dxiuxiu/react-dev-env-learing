766b57ccaa4c14ba008fe89fc21018ef8516dffc
dxiuxiu <37434808+dxiuxiu@users.noreply.github.com>
2018-10-16 14:57:51 +0800

    webpack.config.js：解决导入自定义包不识别问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
-const path = require('path')
+const path = require("path");
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
 const { CheckerPlugin } = require('awesome-typescript-loader')
 module.exports = {
   mode: 'development',
-  // 入口文件设置
   entry: {
-    index: ['./src/index.tsx'], // 入口文件，若不配置将自动查找src目录下的index.js文件
+    index: ["./src/index.tsx"], //入口文件，若不配置webpack4将自动查找src目录下的index.js文件
   },
   output: {
-    filename: 'js/[name].bundle.js', //打包文件输出文件名，[name]表示入口文件js名，默认为main.js
-    path: path.resolve(__dirname, 'build/dist/'), // 打包文件输出路径,默认为项目文件夹/dist
+    filename: "js/[name].bundle.js", //输出文件名，[name]表示入口文件js名
+    path: path.resolve(__dirname, 'build/dist/'), // 输出路径
   },
   resolve: {
-    extensions: ['.js', 'jsx','ts','tsx'], // 作用:这里配置过后缀名的文件在引入时不需要加后缀名
+    extensions: [".ts", ".tsx", ".js", "jsx"],
   },
-  devtool: 'cheap-module-eval-source-map', // 
+  devtool:  'cheap-module-eval-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        exclude: /node_modules/
+        loader: "awesome-typescript-loader"
       }
     ]
   },
